ddb8149677c75ebe1eb8305c2b124d413b8873e4
matspe24 <matspe24@bergen.org>
2023-05-28 19:37:16 -0400

    Added currency + health UI

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
+    public int playerHealth;
+    public int currencyAmount;
+    public Text healthText;
+    public Text currencyText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHealth = 100;
+        currencyAmount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthText.text = playerHealth.ToString();
+        currencyText.text = currencyAmount.ToString();
     }
 }
